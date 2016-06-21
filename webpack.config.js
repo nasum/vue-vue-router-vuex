52851bdf8f327bb6eb576cdd78ff73eea72055b5
nasum <masaya.nasu@crowdworks.co.jp>
2016-06-21 19:47:05 +0900

    setup build system

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  entry: __dirname + "/src/index.js",
+  output: {
+    path: __dirname + "/dist",
+    filename: "bundle.js"
+  },
+
+  module: {
+    loaders: [
+      {
+        test: /\.vue$/,
+        loader: 'vue'
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query:{
+          presets: ['es2015']
+        }
+      }
+    ]
+  }
+}
